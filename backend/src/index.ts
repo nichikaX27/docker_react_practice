9d50ebe126eba7bb1db08c7f5392dfcaa2933435
nichikaX27 <pochinichi@gmail.com>
2026-01-30 09:31:46 +0900

    7.クライアントとサーバーをつなぎこむ

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,23 +1,33 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { cors } from "hono/cors"; // 追加
 
 interface Todo {
   id: number;
   title: string;
   completed: boolean;
 }
+
 const todos: Todo[] = [];
-const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono();
 
-app.get('/todos', (c) => {
+// 追加
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/todos", (c) => {
   return c.json({ todos });
 });
 
-app.post('/todos', async (c) => {
+app.post("/todos", async (c) => {
   const { title } = await c.req.json();
   const todo: Todo = {
     id: todos.length + 1,
@@ -25,25 +35,27 @@ app.post('/todos', async (c) => {
     completed: false,
   };
   todos.push(todo);
-  return c.json({todo});
+  return c.json({ todo });
 });
 
-
-app.put('/todos/:id', async(c) => {
-  const id = c.req.param();
-  const completed = await c.req.json();
+app.put("/todos/:id", async (c) => {
+  const { id } = c.req.param();
+  const { completed } = await c.req.json();
   const todo = todos.find((todo) => todo.id === Number(id));
-if(!todo) {
-  return c.notFound();
-}
+  if (!todo) {
+    return c.notFound();
+  }
   todo.completed = completed;
-  return c.json({todo});
+  return c.json({ todo });
 });
 
-serve({
-  fetch: app.fetch,
-  port: 3001,
-  hostname: '0.0.0.0'
-}, (info) => {
-  console.log(`Server is running on http://0.0.0.0:${info.port}`)
-})
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+    hostname: "0.0.0.0",
+  },
+  (info) => {
+    console.log(`Server is running on http://0.0.0.0:${info.port}`);
+  }
+);
