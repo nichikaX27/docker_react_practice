b2dd5b7c9b3517c769b4a4bfbd92535cad148957
nichikaX27 <pochinichi@gmail.com>
2026-01-30 14:19:24 +0900

    チュートリアル完了、次回発展課題へ

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(
+  
+  {
+    server: {
+    watch: {
+      usePolling: true, // これを追加
+    },
+    host: '0.0.0.0',
+  },
   plugins: [react(), tailwindcss()],
 });
